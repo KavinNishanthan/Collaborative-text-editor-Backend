976c8f995ad1923b5f2df7fd1f8f0afd48aec8f4
KavinNishanthan <kavinnishanthan2004@gmail.com>
2026-04-02 16:41:14 +0530

    pushed untracked file

--- src/routes/document.route.ts
+++ src/routes/document.route.ts
@@ -7,16 +7,13 @@ import documentController from '../controllers/document.controller';
 // Importing middleware
 import authMiddleware from '../middlewares/auth.middleware';
 
-/**
- * @createdBy Kavin Nishanthan P D
- * @createdAt 2026-04-01
- * @description Routes for document CRUD operations
- */
 
 const router = Router();
 
 router.use(authMiddleware);
 
+
+// Doc Route
 router.post('/', documentController.handleCreateDocument);
 
 
