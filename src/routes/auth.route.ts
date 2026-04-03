77027b4727f2843cbfd4170e96e5efba2d479b6d
KavinNishanthan <kavinnishanthan2004@gmail.com>
2026-04-03 23:45:44 +0530

    Implemented Logout Route.

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,6 +4,9 @@ import { Router } from 'express';
 // Importing controllers
 import authController from '../controllers/auth.controller';
 
+// Importing middleware
+import authMiddleware from '../middlewares/auth.middleware';
+
 // Defining routers
 const router = Router();
 
@@ -13,5 +16,10 @@ router.post('/verify/otp', authController.handleVerifyOtpAndRegister);
 
 // Manual Login routes
 router.get('/login', authController.handleLogin);
+
+router.post('/logout', authMiddleware, (req, res) => {
+  res.clearCookie('token', { httpOnly: true, secure: true, sameSite: 'strict' });
+  res.status(200).json({ status: 'Ok', code: 200, message: 'Logged out successfully!' });
+});
 
 export default router;
