19f69bae060ea6ef777cdcb7355f9524ff86cbe9
KavinNishanthan <kavinnishanthan2004@gmail.com>
2026-04-04 14:01:04 +0530

    updated cors

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ socketConfig(io);
 
 app.use(
   cors({
-    origin: corsOrigins,
+    origin: [...corsOrigins, 'http://localhost:5173'],
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true
   })
