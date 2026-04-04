9b0fabcdd0af08707d193f9bc397ee8a6eb52c7f
KavinNishanthan <kavinnishanthan2004@gmail.com>
2026-04-04 21:02:48 +0530

    fixed cookie bug.

--- src/helpers/cookie.helper.ts
+++ src/helpers/cookie.helper.ts
@@ -16,7 +16,7 @@ const setAuthCookie = (res: Response, userId: string, email: string): void => {
 
   res.cookie('token', token, {
     httpOnly: true,
-    sameSite: 'none',
+    sameSite: 'lax',
     secure: false,
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
